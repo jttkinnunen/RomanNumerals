858c1f11ffbb8e95ad3ad9149fb8664a7120a2b0
jttkinnunen <jttkinnunen@gmail.com>
2018-10-10 18:07:54 +0300

    Tst

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,143 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+/*Rules:
+There were certain rules that the numerals followed which should be observed:
+•The symbols 'I', 'X', 'C', and 'M' can be repeated at most 3 times in a row.
+•The symbols 'V', 'L', and 'D' can never be repeated.
+•The '1' symbols ('I','X', and 'C') can only be subtracted from 
+the 2 next highest values ('IV' and  'IX',  'XL' and 'XC', 'CD' and 'CM').
+•Only one subtraction can be made per numeral ('XC' is allowed, 'XXC' is not).
+•The '5' symbols ('V', 'L', and 'D') can never be subtracted*/
+
+
+
 
 public class RomanNumerals {
 	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
+		
+			String redString1 = romanNum;
+			boolean empty = redString1.isEmpty();
+				if(empty) {
+					return 0;
+				}
+			boolean hadCommaSlash = redString1.contains(",\n");
+			if(hadCommaSlash) {
+				return -1;
+			}		
+			//String delims = "[,\n]";
+			String delims = "";
+			String[] numbers = redString1.split(delims);
+			
+			int lukuja = numbers.length;
+			int summa = 0;
+			String number = "";
+			for (int i = 0; i < lukuja ; i++ ) {
+				char[] c = numbers[i].toCharArray();
+				int  merkkeja = c.length;
+				 number = ""; 
+				if(Character.isAlphabetic(c[merkkeja-1])) {
+					summa = parseArabic(numbers);
+					
+					
+				}else if (Character.isDigit(c[merkkeja-1])) {
+					//do nothing
+				//summa = summa+ Integer.parseInt(numbers[i]);
+				}
+			}
+			summa = Integer.parseInt(number);
+			return summa;
+			
+				
+	}
+	public int parseArabic(String[] romans) {
+		String [] RomanNumbers = romans;
+		int merkkeja = RomanNumbers.length; 
+		int number =0;
+		if(merkkeja < 2) {
+			if(RomanNumbers[1] == "I" && merkkeja < 2) {
+				 number = number + 1;
+			}
+			if(RomanNumbers[1] == "V") {
+				 number = number + 5;
+			}
+			if(RomanNumbers[1] == "X") {
+				 number = number + 10;
+			}
+			if(RomanNumbers[1] == "C") {
+				 number = number + 100;
+			}
+			if(RomanNumbers[1] == "D") {
+				 number = number + 500;
+			}
+			if(RomanNumbers[1] == "M") {
+				 number = number + 1000;
+			}
+		}
+		
+		
+		int conventerRoman = number;
+		return conventerRoman;
 		
 	}
+	
+	public static int romanToArabic(String input) {
+        String romanNumeral = input.toUpperCase();
+        int result = 0;
+        int occured = 0;
+        
+        List<RomanNumeral> romanNumerals = RomanNumeral.getReverseSortedValues();
+
+        int i = 0;
+
+        while ((romanNumeral.length() > 0) && (i < romanNumerals.size())) {
+        	RomanNumeral symbol = romanNumerals.get(i);
+        	if (i>1) {
+        		RomanNumeral symbol2 = romanNumerals.get(i-1);
+        		//RomanNumeral symbol1 = romanNumerals.get(i-1);
+        		if( symbol2 == symbol) {
+        			occured ++;
+        		}
+        		if(occured > 3) {
+        			throw new IllegalArgumentException(input + " too many same Letters");
+        		}
+        	}
+             
+            if (romanNumeral.startsWith(symbol.name())) {
+                result += symbol.getValue();
+                romanNumeral = romanNumeral.substring(symbol.name().length());
+            } else {
+                i++;
+            }
+        }
+        if (romanNumeral.length() > 0) {
+            throw new IllegalArgumentException(input + " cannot be converted to a Roman Numeral");
+        }
+
+        return result;
+	}
+	enum RomanNumeral {
+	    I(1), IV(4), V(5), IX(9), X(10), XL(40), L(50), XC(90), C(100), CD(400), D(500), CM(900), M(1000);
+
+	    private int value;
+
+	    RomanNumeral(int value) {
+	        this.value = value;
+	    }
+
+	    public int getValue() {
+	        return value;
+	    }
+	    
+	    public static List<RomanNumeral> getReverseSortedValues() {
+	        return Arrays.stream(values())
+	          .sorted(Comparator.comparing((RomanNumeral e) -> e.value).reversed())
+	          .collect(Collectors.toList());
+	    }
+	}
+		
 }
+
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -5,8 +5,75 @@ import org.junit.Test;
 public class TestRomanNumerals {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testEmpty()  {
+		RomanNumerals convert = new RomanNumerals();
+		
+		int result = convert.convertToInteger("");
+		// Assert
+		assertEquals("No empty string", 0, result);
 	}
-
+	@Test
+	public void testI()  {
+		RomanNumerals convert = new RomanNumerals();
+		
+		int result = convert.romanToArabic("I");
+		// Assert
+		assertEquals("Can not find I", 1, result);
+	}
+	@Test
+	public void testV()  {
+		RomanNumerals convert = new RomanNumerals();
+		
+		int result = convert.romanToArabic("V");
+		// Assert
+		assertEquals("Can not find V", 5, result);
+	}
+	@Test
+	public void testX()  {
+		RomanNumerals convert = new RomanNumerals();
+		
+		int result = convert.romanToArabic("X");
+		// Assert
+		assertEquals("Can not find X", 10, result);
+	}
+	@Test
+	public void testC()  {
+		RomanNumerals convert = new RomanNumerals();
+		
+		int result = convert.romanToArabic("C");
+		// Assert
+		assertEquals("Can not find X", 100, result);
+	}
+	@Test
+	public void testD()  {
+		RomanNumerals convert = new RomanNumerals();
+		
+		int result = convert.romanToArabic("D");
+		// Assert
+		assertEquals("Can not find X", 500, result);
+	}
+	@Test
+	public void testM()  {
+		RomanNumerals convert = new RomanNumerals();
+		
+		int result = convert.romanToArabic("M");
+		// Assert
+		assertEquals("Can not find M", 1000, result);
+	}
+	@Test
+	public void testMMC()  {
+		RomanNumerals convert = new RomanNumerals();
+		
+		int result = convert.romanToArabic("MMC");
+		// Assert
+		assertEquals("Can not find M", 2100, result);
+	}
+	@Test
+	public void testIIII()  {
+		RomanNumerals convert = new RomanNumerals();
+		
+		int result = convert.romanToArabic("IIII");
+		// Assert
+		assertEquals("Can not find M", -1, result);
+	}
 }
